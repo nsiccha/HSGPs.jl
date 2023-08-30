c57f6edb4525d7ffec77912d41a7e2e4139fc572
Nikolas Siccha <nikolas.siccha@aalto.fi>
2023-08-30 17:19:41 +0300

    autocommit

--- ext/HSGPsWarmupHMCExt.jl
+++ ext/HSGPsWarmupHMCExt.jl
@@ -10,17 +10,28 @@ WarmupHMC.reparametrize(source::HSGP, parameters::AbstractVector) = HSGP(
     collect.(eachcol(reshape(parameters, (:, 2))))...
 )
 
-@views WarmupHMC.reparametrize(source::HSGP, target::HSGP, draw::AbstractVector) = begin 
+# @views WarmupHMC.reparametrize(source::HSGP, target::HSGP, draw::AbstractVector) = begin 
+#     sxic = draw[4:end]
+#     lsds = HSGPs.log_sds(source, draw)
+#     w = sxic .* exp.(lsds .* (1 .- source.centeredness))
+#     txic = sxic .* exp.(lsds .* (target.centeredness .- source.centeredness))
+#     trintercept = draw[1] + sum(w .* (target.mean_shift - source.mean_shift))
+#     vcat(trintercept, draw[2:3], txic)
+# end
+
+# @views WarmupHMC.lja(source::HSGP, target::HSGP, draw::AbstractVector) = begin 
+#     -sum(HSGPs.log_sds(source, draw) .* target.centeredness)
+# end
+
+@views WarmupHMC.lja_reparametrize(source::HSGP, target::HSGP, draw::AbstractVector, lja=0.) = begin  
     sxic = draw[4:end]
     lsds = HSGPs.log_sds(source, draw)
     w = sxic .* exp.(lsds .* (1 .- source.centeredness))
     txic = sxic .* exp.(lsds .* (target.centeredness .- source.centeredness))
     trintercept = draw[1] + sum(w .* (target.mean_shift - source.mean_shift))
-    vcat(trintercept, draw[2:3], txic)
-end
-
-@views WarmupHMC.lja(source::HSGP, target::HSGP, draw::AbstractVector) = begin 
-    -sum(HSGPs.log_sds(source, draw) .* target.centeredness)
+    lja += -sum(HSGPs.log_sds(source, draw) .* target.centeredness)
+    tdraw = vcat(trintercept, draw[2:3], txic)
+    lja, tdraw
 end
 
 WarmupHMC.logdensity_and_stuff(source::HSGP, draw::AbstractVector) = logpdf_and_y(source, draw)
